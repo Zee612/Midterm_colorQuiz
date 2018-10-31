3493f643159b83bddc1d28599999c2504bd98b9e
Zee's MAC <zeesmac@ZeesdeMacBook-Pro.local>
2018-10-30 17:46:44 -0700

    update 2

--- color-quiz.js
+++ color-quiz.js
@@ -16,7 +16,7 @@ let questions = [
     {
         question : " What color is not shown in the circles?",
         imgSrc : "img/picture1.png",
-        choiceA : "A.Magentat",
+        choiceA : "A.Magenta",
         choiceB : "B.Green",
         choiceC : "C.White",
         correct : "C"
@@ -39,7 +39,7 @@ let questions = [
       imgSrc : "img/picture4.jpg",
       choiceA : "A.Warm-toned",
       choiceB : "B.Cool-toned",
-
+      choiceC:  "C.Neither"
       correct : "B"
 
     },{
